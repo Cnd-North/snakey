a8859bef8f73fcc1a0e8be9d9c23be670eb3025a
Cnd-North <gerrit.vr12@gmail.com>
2017-03-01 22:06:26 -0800

    Cleaning Up
    Cleaning up some unnecessaries

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
 # Group Snake Project
 
-
-# The link is example code of a python snake with decent commenting:
-# https://github.com/zevisert/battlesnake2016/blob/master/app/main.py
-
 import bottle
 import os
 import random
